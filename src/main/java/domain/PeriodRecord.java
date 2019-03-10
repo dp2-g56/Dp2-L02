ca158d8cabb74e7aaa2978d74c57ed635c22f8b6
daniarcaa <danielnf@hotmail.es>
2019-03-10 21:07:42 +0100

    History domain (Falta populate)

---
+++ src/main/java/domain/PeriodRecord.java
@@ -0,0 +1,75 @@
+package domain;
+
+import java.util.List;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class PeriodRecord extends DomainEntity {
+
+	private String title;
+	private String description;
+	private Integer startYear;
+	private Integer endYear;
+	private List<String> photos;
+
+	public PeriodRecord() {
+		super();
+	}
+
+	@NotBlank
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@NotBlank
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@NotNull
+	@Valid
+	public Integer getStartYear() {
+		return this.startYear;
+	}
+
+	public void setStartYear(Integer startYear) {
+		this.startYear = startYear;
+	}
+
+	@Valid
+	public Integer getEndYear() {
+		return this.endYear;
+	}
+
+	public void setEndYear(Integer endYear) {
+		this.endYear = endYear;
+	}
+
+	@Valid
+	@ElementCollection(targetClass = String.class)
+	public List<String> getPhotos() {
+		return this.photos;
+	}
+
+	public void setPhotos(List<String> photos) {
+		this.photos = photos;
+	}
+
+}

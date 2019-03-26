291a07314f04ff85505792e6796e0126fac7826d
daniarcaa <danielnf@hotmail.es>
2019-03-26 17:18:48 +0100

    Pequeño arreglo optimización

--- src/main/java/domain/Parade.java
+++ src/main/java/domain/Parade.java
@@ -27,8 +27,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
-@Table(indexes = { @Index(columnList = "isDraftMode, moment"), @Index(columnList = "title, description"),
-		@Index(columnList = "moment"), @Index(columnList = "isDraftMode"), @Index(columnList = "paradeStatus") })
+@Table(indexes = { @Index(columnList = "isDraftMode, moment"), @Index(columnList = "paradeStatus, isDraftMode"),
+		@Index(columnList = "title, description"), @Index(columnList = "moment"), @Index(columnList = "isDraftMode"),
+		@Index(columnList = "paradeStatus") })
 public class Parade extends DomainEntity {
 
 	private String title; //
